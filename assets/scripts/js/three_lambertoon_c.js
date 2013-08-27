3c70688c93c32b39075cc51e0ea268537f0a59ce
lettier <contact@lettier.com>
2013-08-27 11:37:29 -0400

    Commented cel-shader.

--- assets/scripts/js/three_lambertoon_c.js
+++ assets/scripts/js/three_lambertoon_c.js
@@ -11,20 +11,20 @@
  *
  */
 
-THREE.ShaderLib['lambert'].fragmentShader = THREE.ShaderLib['lambert'].fragmentShader.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-THREE.ShaderLib['lambert'].fragmentShader = "uniform vec3 diffuse;\n" + THREE.ShaderLib['lambert'].fragmentShader.substr(0, THREE.ShaderLib['lambert'].fragmentShader.length-1);
-THREE.ShaderLib['lambert'].fragmentShader += [
+THREE.ShaderLib['lambert'].fragmentShader = THREE.ShaderLib['lambert'].fragmentShader.replace(/^\s\s*/, '').replace(/\s\s*$/, ''); // Remove white-space.
+THREE.ShaderLib['lambert'].fragmentShader = "uniform vec3 diffuse;\n" + THREE.ShaderLib['lambert'].fragmentShader.substr(0, THREE.ShaderLib['lambert'].fragmentShader.length-1); // Add a string to the top of the shader.
+THREE.ShaderLib['lambert'].fragmentShader += [ // Augment the bottom of the shader string(s) with the following cel-shading logic strings.
 	"#ifdef USE_MAP",
-	//"	gl_FragColor = texture2D( map, vUv );",
+	//"	gl_FragColor = texture2D( map, vUv );", // Commented out to included shadow map color.
 	"	gl_FragColor = gl_FragColor;",
 	"#else",
 	"	gl_FragColor = vec4(diffuse, 1.0);",
 	"#endif",
-	"	vec3 basecolor = vec3(gl_FragColor[0], gl_FragColor[1], gl_FragColor[2]);",
-	"	float alpha = gl_FragColor[3];",
-	"	float vlf = vLightFront[0];",
+	"	vec3 basecolor = vec3(gl_FragColor[0], gl_FragColor[1], gl_FragColor[2]);", // Base color before cel-shading.
+	"	float alpha = gl_FragColor[3];", // Alpha value of fragment.
+	"	float vlf = vLightFront[0];", // Front facing light intensity.
 	// Clean and simple //
-	"	if (vlf< 0.50) { gl_FragColor = vec4(mix( basecolor, vec3(0.0), 0.5), alpha); }",
+	"	if (vlf< 0.50) { gl_FragColor = vec4(mix( basecolor, vec3(0.0), 0.5), alpha); }", // Discretize the light to three bands.
 	"	if (vlf>=0.50) { gl_FragColor = vec4(mix( basecolor, vec3(0.0), 0.3), alpha); }",
 	"	if (vlf>=0.75) { gl_FragColor = vec4(mix( basecolor, vec3(1.0), 0.0), alpha); }",
 	//"	if (vlf>=0.95) { gl_FragColor = vec4(mix( basecolor, vec3(1.0), 0.3), alpha); }",
